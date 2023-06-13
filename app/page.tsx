5f521039d8db7ec8b1a75e75b80ebfce45a6937f
nickundin <nickundin@gmail.com>
2023-06-13 09:59:32 -0400

    Added gradient background container

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+'use client';
+
 import Image from 'next/image';
-import styles from './page.module.css';
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      {/* <div>Hello, world!</div>
-      <h3>Great job running Next.js!</h3> */}
+    <main>
+      {/* Background */}
+      <GradientBackgroundCon></GradientBackgroundCon>
     </main>
   );
 }
