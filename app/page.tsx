34bc4a7a42ee41442cab1165beddb6220031a121
nickundin <nickundin@gmail.com>
2023-06-12 21:06:03 -0400

    Set up Next.js project with styled components

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      {/* <div>Hello, world!</div>
+      <h3>Great job running Next.js!</h3> */}
+    </main>
+  );
+}
